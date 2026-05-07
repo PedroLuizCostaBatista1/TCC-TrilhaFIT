61bf2b84b44b35938e7150bdf677f549f67ca8fe
Pedro Luiz <0001156035@senaimgaluno.com.br>
2026-05-07 08:45:06 -0300

    Redesign da tela de boas vindas

--- resources/views/boasvindas.blade.php
+++ resources/views/boasvindas.blade.php
@@ -5,29 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com/3.4.17"></script>
     <script src="https://cdn.jsdelivr.net/npm/lucide@0.263.0/dist/umd/lucide.min.js"></script>
-    <link rel="stylesheet" href="/assets/css/login.css">
+    <link rel="stylesheet" href="/assets/css/boasvindas.css">
     <title>TrilhaFIT</title>
 </head>
 <body>
-    <div class="app-wrapper">
-        <div class="tela">
-            <div id="logo" class="anim-in anim-d1">
-                <div id="borda">
-                    <i data-lucide="bike" id="icone"></i>
-                </div>
-                <h1 class="titulo">Bem-vindo ao TrilhaFIT!</h1>
-                <p class="subtitulo">Pedale, evolua e vença! 🚴‍♂️</p>
-            </div>
-            <div class="anim-in anim-d2 botoes">
-                <a href="cadastro" class="botao1">
-                    <span>Criar Conta</span>
-                </a>
-                <a href="login" class="botao2">
-                    <span>Entrar</span>
-                </a>
-            </div>
-        </div>
-    </div>
+    <main class="tela">
+        <header id="logo" class="anim-in anim-d1">
+            <figure id="borda">
+                <i data-lucide="bike" id="icone"></i>
+            </figure>
+            <h1 class="titulo">Bem-vindo ao TrilhaFIT!</h1>
+            <p class="subtitulo">Pedale, evolua e vença!</p>
+        </header>
+        <section class="anim-in anim-d2 botoes">
+            <a href="cadastro" class="botao1">Criar Conta</a>
+            <a href="login" class="botao2">Entrar</a>
+        </section>
+    </main>
     <script>
         lucide.createIcons();
     </script>
