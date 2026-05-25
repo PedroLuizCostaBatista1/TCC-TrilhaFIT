edc540e7520e5098e1a547a627774e031e42bcc4
Pedro Luiz <0001156035@senaimgaluno.com.br>
2026-05-25 08:56:39 -0300

    Conserto do bug de rotas

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,20 @@ Route::get('/', function () {
 Route::middleware('guest')->group(function () {
     Route::get('/cadastro', [UsuarioController::class, 'telaCadastro'])->name('cadastro');
     Route::post('/cadastro', [UsuarioController::class, 'store']);
-
-    Route::get('/login', [AuthController::class, 'telaLogin'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
-
-    Route::get('/esqueci-senha', [AuthController::class, 'telaEsqueciSenha'])->name('trocar-senha');
-    Route::post('/esqueci-senha', [AuthController::class, 'verificarEmail'])->name('verificar-email');
-
-    Route::get('/verificar-codigo', [AuthController::class, 'telaVerificarCodigo'])->name('verificar-codigo');
-    Route::post('/verificar-codigo', [AuthController::class, 'validarCodigo'])->name('validar-codigo');
-    Route::post('/reenviar-codigo', [AuthController::class, 'reenviarCodigo'])->name('reenviar-codigo');
-
-    Route::get('/redefinir-senha', [AuthController::class, 'telaRedefinirSenha'])->name('redefinir-senha');
-    Route::post('/redefinir-senha', [AuthController::class, 'atualizarSenha'])->name('atualizar-senha');
 });
+
+Route::get('/login', [AuthController::class, 'telaLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/esqueci-senha', [AuthController::class, 'telaEsqueciSenha'])->name('trocar-senha');
+Route::post('/esqueci-senha', [AuthController::class, 'verificarEmail'])->name('verificar-email');
+
+Route::get('/verificar-codigo', [AuthController::class, 'telaVerificarCodigo'])->name('verificar-codigo');
+Route::post('/verificar-codigo', [AuthController::class, 'validarCodigo'])->name('validar-codigo');
+Route::post('/reenviar-codigo', [AuthController::class, 'reenviarCodigo'])->name('reenviar-codigo');
+
+Route::get('/redefinir-senha', [AuthController::class, 'telaRedefinirSenha'])->name('redefinir-senha');
+Route::post('/redefinir-senha', [AuthController::class, 'atualizarSenha'])->name('atualizar-senha');
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
