4cb2750dde818cf199985a8766d5e22741594961
Pedro Luiz <0001156035@senaimgaluno.com.br>
2026-05-28 10:24:44 -0300

    Continuacao em casa

--- resources/views/dashboard/relatorio.blade.php
+++ resources/views/dashboard/relatorio.blade.php
@@ -3,12 +3,15 @@
 @push("css")
     <link rel="stylesheet" href="/assets/css/dashboard/relatorio.css">
 @endpush
-<main class="tela">
+<main>
     <header>
-        <h1 id="titulo">Última sessão</h1>
-        <p id="horario">Hoje às 14:30</p>
+        <h1>Última sessão</h1>
+        <div>
+            <span id="mural-icone" class="material-symbols-outlined">schedule</span>
+            <p>Hoje às 14:30</p>
+        </div>
     </header>
-    <section id="sessao">
+    <section id="relatorio">
         <div class="card">
             <p class="card-titulo">RPM</p>
             <p class="card-valor">85</p>
@@ -20,24 +23,18 @@
             <p class="card-tipo">quilômetros</p>
         </div>
         <div class="card">
-            <p class="card-titulo">Calorias</p>
-            <p class="card-valor">347</p>
-            <p class="card-tipo">kcal queimadas</p>
+            <div class="card-titulo">
+                <span id="mural-icone" class="material-symbols-outlined">mode_heat</span>
+                <p>Calorias</p>
+            </div>
+            <h3 class="card-valor">347 <span>kcal</span></h3>
         </div>
         <div class="card">
-            <p class="card-titulo">Duração</p>
-            <p class="card-valor">45</p>
-            <p class="card-tipo">minutos</p>
-        </div>
-        <div class="card">
-            <p class="card-titulo">Velocidade Média</p>
-            <p class="card-valor">16.7</p>
-            <p class="card-tipo">quilômetros</p>
-        </div>
-        <div class="card">
-            <p class="card-titulo">Altitude Ganho</p>
-            <p class="card-valor">245</p>
-            <p class="card-tipo">metros</p>
+            <div class="card-titulo">
+                <span id="mural-icone" class="material-symbols-outlined">speed</span>
+                <p>Vel. Média</p>
+            </div>
+            <h3 class="card-valor">16.70 <span>km/h</span></h3>
         </div>
     </section>
     <section id="botoes">
